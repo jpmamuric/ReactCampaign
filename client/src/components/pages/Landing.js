99aa12c7074ab544f57c3b0de92e9d5f0b6adfd9
Jan Peter Mamuric <webdev.jmamuric@gmail.com>
2018-03-31 09:24:57 -0700

    fixed minor duplication in landing page

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -12,8 +12,6 @@ const Landing = () => {
         <h2> Welcome! </h2>
         <p style={{ padding: '0 1.5em'}}>Collect feedback from your users emails.</p>
       </div>
-      <h2> Welcome! </h2>
-      <p style={{ padding: '0 1.5em'}}>Use React Campaign to collect feedback from your users emails</p>
     </div>
   )
 }
